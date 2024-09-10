4b3cce4fd291cd8844241a8017ec08a07a2d7291
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 02:44:48 +0300

    create role_permissions migration

---
+++ database/migrations/2024_09_10_234153_create_role_permission_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('role_permission', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->string('permission');
+            $table->enum('type', ['Team', 'Mentor']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('role_permission');
+    }
+};

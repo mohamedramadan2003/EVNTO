92927334d3a664873430d209a3ffed2a381661b0
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 02:28:14 +0300

    create user_skills migration

---
+++ database/migrations/2024_09_10_232552_create_user_skills_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_skills', function (Blueprint $table) {
+            $table->id();
+            $table->string('skill');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_skills');
+    }
+};

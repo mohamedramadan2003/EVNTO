1a7e668ec36d1142fcf037fa5fa446fcba6a6e13
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 16:38:18 +0300

    add OrganizerProfile Model

---
+++ app/Models/OrganizerProfile.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrganizerProfile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'image', 'facebook_link', 'linkedin_link', 'twitter_link', 'organizer_id',
+    ];
+
+    public function organizer()
+    {
+        return $this->belongsTo(Organizer::class);
+    }
+}

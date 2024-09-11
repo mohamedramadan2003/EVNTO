106eb019ed39dfcdec4bf920203315f7d0f90882
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 03:08:29 +0300

    create events migration

---
+++ database/migrations/2024_09_10_235857_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->text('description');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->time('time');
+            $table->enum('type', ['paid', 'free']);
+            $table->enum('status', ['upcoming', 'past']);
+            $table->string('category');
+            $table->integer('rating')->nullable();
+            $table->string('booking_link');
+            $table->foreignId('organizer_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+};

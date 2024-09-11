2f3ebb5273fadc112bd2684d0759b3d69ae8bfc2
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 16:10:22 +0300

    add UserProfile Model

---
+++ app/Models/UserProfile.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'collage_name', 'image', 'user_id',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}

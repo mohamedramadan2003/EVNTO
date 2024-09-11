52720dffcc2350cf4779ba8f3e2ac916095c5b9b
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 19:13:00 +0300

    update User Model

--- app/Models/User/User.php
+++ app/Models/User/User.php
@@ -3,6 +3,8 @@
 namespace App\Models\User;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Event\Event;
+use App\Models\Event\EventComment;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,6 +22,36 @@ class User extends Authenticatable
     protected $fillable = [
         'name', 'email', 'password', 'provider', 'provider_id', 'provider_token',
     ];
+
+    public function profile()
+    {
+        return $this->hasOne(UserProfile::class);
+    }
+
+    public function skills()
+    {
+        return $this->hasMany(UserSkill::class);
+    }
+
+    public function interests()
+    {
+        return $this->hasMany(UserInterest::class);
+    }
+
+    public function notifications()
+    {
+        return $this->hasMany(Notification::class);
+    }
+
+    public function favouriteEvents()
+    {
+        return $this->belongsToMany(Event::class, 'favourite_events');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(EventComment::class);
+    }
 
     /**
      * The attributes that should be hidden for serialization.

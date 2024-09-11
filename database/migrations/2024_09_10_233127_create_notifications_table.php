c098d7d524276eedd197c2470e0d433620aa75a0
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-11 15:55:33 +0300

    update motifications table

--- database/migrations/2024_09_10_233127_create_notifications_table.php
+++ database/migrations/2024_09_10_233127_create_notifications_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
-            $table->enum('status', ['unread', 'read']);
+            $table->enum('status', ['unread', 'read'])->default('unread');
             $table->text('data');
             $table->timestamp('read_at')->nullable();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');

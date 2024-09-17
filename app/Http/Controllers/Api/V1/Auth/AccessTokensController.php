0b54b28241f282f89f7178c299c143de76864cc0
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-17 18:14:02 +0300

    make Registeration method

--- app/Http/Controllers/Api/V1/Auth/AccessTokensController.php
+++ app/Http/Controllers/Api/V1/Auth/AccessTokensController.php
@@ -3,14 +3,38 @@
 namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\Auth\RegisterRequest;
 use App\Models\User\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AccessTokensController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
+        if (User::where('email', $request->email)->exists()) {
+            return response([
+                'message' => 'Email already exists',
+                'status' => 'failed'
+            ], 409);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('apptoken')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+            'message' => 'Registration Success',
+            'status' => 'success'
+        ];
+
+        return response($response, 201);
 
     }
     public function login(Request $request)

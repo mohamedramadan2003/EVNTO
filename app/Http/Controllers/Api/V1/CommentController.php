38be49e218acb6756d294bf8125b2baf4ab277c3
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-18 03:18:30 +0300

    create update comment method

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\api\EventComment\StoreEventCommentRequest;
+use App\Http\Requests\api\EventComment\UpdateEventCommentRequest;
 use App\Models\Event\EventComment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -26,8 +27,18 @@ class CommentController extends Controller
         return response()->json($comment, 201);
     }
 
-    public function update(Request $request)
+    public function update(UpdateEventCommentRequest $request, $id)
     {
+        $comment = EventComment::findOrFail($id);
+
+        // Check if the authenticated user is the owner of the comment
+        if ($comment->user_id !== Auth::id()) {
+            return response()->json(['message' => 'Unauthorized'], 403);
+        }
+
+        $comment->update($request->only('description'));
+
+        return response()->json($comment);
 
     }
     public function delete(Request $request , string $id)

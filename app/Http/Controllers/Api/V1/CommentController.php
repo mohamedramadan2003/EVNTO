e093acf3d50c29dc293851521b327d3d48e87767
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-18 03:09:27 +0300

    Create add comment method

--- app/Http/Controllers/Api/V1/CommentController.php
+++ app/Http/Controllers/Api/V1/CommentController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\api\EventComment\StoreEventCommentRequest;
+use App\Models\Event\EventComment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
     public function index()
     {
 
+    }
+
+    public function store(StoreEventCommentRequest $request)
+    {
+        $comment = EventComment::create([
+            'description' => $request->description,
+            'user_id' => Auth::id(),
+            'event_id' => $request->event_id,
+        ]);
+
+        return response()->json($comment, 201);
     }
 
     public function update(Request $request)

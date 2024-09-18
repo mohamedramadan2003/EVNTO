cf4b93eb80140646036ce454a3f2a6d8526f01bb
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-19 02:00:42 +0300

    add UserInterestSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             UserSkillSeeder::class,
+            UserInterestSeeder::class,
         ]);
         // \App\Models\User::factory(10)->create();
 

0c9491d71583db6cb4e9087c9079c36e10c26db3
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-20 00:39:27 +0300

    add SearchEvent Request

---
+++ app/Http/Requests/api/SearchEvent/SearchEventRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\api\SearchEvent;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SearchEventRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'query' => 'nullable|string|max:255',
+            'organizer_name' => 'nullable|string|max:255',
+            'address' => 'nullable|string|max:255',
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'query.string' => 'The search query must be a string.',
+            'organizer_name.string' => 'The organizer name must be a string.',
+            'address.string' => 'The address must be a string.',
+        ];
+    }
+}

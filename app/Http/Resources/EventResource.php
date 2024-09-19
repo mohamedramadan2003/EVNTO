4e191eadfa9b24001fd4b9f7ecb934fc43260413
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-19 17:04:17 +0300

    update EventResource

--- app/Http/Resources/EventResource.php
+++ app/Http/Resources/EventResource.php
@@ -14,6 +14,24 @@ class EventResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+//        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'image' => $this->image,
+            'description' => $this->description,
+            'start_date' => $this->start_date,
+            'end_date' => $this->end_date,
+            'time' => $this->time,
+            'type' => $this->type,
+            'status' => $this->status,
+            'category' => $this->category,
+            'rating' => $this->rating,
+            'booking_link' => $this->booking_link,
+            'organizer_id' => $this->organizer_id,
+            'speakers' => $this->speakers,
+            'goals' => $this->goals,
+            'location' => $this->location,
+        ];
     }
 }

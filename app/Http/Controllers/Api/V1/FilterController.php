b6d6f0c4b2b7c717ee596d4005fa3124eacafa6c
AmiraMohamed56 <awadet3469@gmail.com>
2024-09-20 01:02:29 +0300

    add filter Method

--- app/Http/Controllers/Api/V1/FilterController.php
+++ app/Http/Controllers/Api/V1/FilterController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\api\SearchEvent\SearchEventRequest;
 use App\Http\Resources\EventResource;
 use App\Models\Event\Event;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class FilterController extends Controller
@@ -48,8 +49,54 @@ class FilterController extends Controller
         return EventResource::collection($events);
     }
 
-    public function filter(Request $request)
+    public function filter(SearchEventRequest $request)
     {
+        $category = $request->input('category');
+        $eventType = $request->input('event_type');
+        $eventDate = $request->input('event_date');
+        $setDate = $request->input('set_date');
 
+        $eventsQuery = Event::query();
+
+        if ($category) {
+            $eventsQuery->where('category', 'LIKE', "%{$category}%");
+        }
+
+        if ($eventType) {
+            $eventsQuery->where('type', $eventType);
+        }
+
+        if ($eventDate) {
+            $today = Carbon::today();
+            $tomorrow = Carbon::tomorrow();
+            $endOfWeek = Carbon::now()->endOfWeek();
+
+            switch ($eventDate) {
+                case 'today':
+                    $eventsQuery->whereDate('start_date', $today);
+                    break;
+                case 'tomorrow':
+                    $eventsQuery->whereDate('start_date', $tomorrow);
+                    break;
+                case 'this_week':
+                    $eventsQuery->whereBetween('start_date', [$today, $endOfWeek]);
+                    break;
+                case 'set_date':
+                    if ($setDate) {
+                        $eventsQuery->whereDate('start_date', $setDate);
+                    }
+                    break;
+            }
+        }
+
+        $events = $eventsQuery->with(['speakers', 'goals', 'location', 'organizer'])->get();
+
+        if ($events->isEmpty()) {
+            return response()->json([
+                'message' => 'No results found.'
+            ], 200);
+        }
+
+        return EventResource::collection($events);
     }
 }
